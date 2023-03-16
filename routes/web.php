8ab485153296683dd0be73091c092ce3dd8a7ebd
fesavali <felixnzioki99@gmail.com>
2023-03-16 12:15:17 +0300

    routes

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+Route::get('/ListYourCar', 'App\Http\Controllers\sellController@index')->name('sellcar');
+Route::get('/LoginPage', 'App\Http\Controllers\buyerController@index')->name('login');
+Route::get('/ListedCars', 'App\Http\Controllers\carController@index')->name('all_cars');
+Route::get('/Contact Us', 'App\Http\Controllers\contactController@index')->name('contact');
+Route::post('/Listed/Results', 'App\Http\Controllers\carController@search')->name('search');
