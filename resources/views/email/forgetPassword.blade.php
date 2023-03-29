facdee07a4805bbf95ae5647622ba791192fd99d
fesavali <felixnzioki99@gmail.com>
2023-03-29 21:18:15 +0300

    email templates

---
+++ resources/views/email/forgetPassword.blade.php
@@ -0,0 +1,21 @@
+<h3>Forget Password Email</h3>
+
+Hello, 
+<br/>
+<br/>
+<br/>
+Thank you for Choosing Car Shop Automobiles.
+<br/>
+<br/>
+You can reset password from this link:<a href="{{ route('reset.password.get', $token) }}">Reset Password</a>
+<br/>
+<br/>
+Visit our Company website <a href="{{ route('home') }}">@Car Shop</a> for more exciting automobiles.
+<br/>
+<br/>
+<br/>
+<br/>
+Kind Regards
+<br/>
+<br/>
+Felix Savali Alx Alumni.
